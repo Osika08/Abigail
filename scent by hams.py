56ac8bd8f864c1ff59f2629c2bfaf40d5f8175a6
osika08 <abigailosika07@g.mail.com>
2024-11-13 10:27:17 +0100

    perfume store

---
+++ scent by hams.py
@@ -0,0 +1,9 @@
+things = ["car", "shoe", "shorts", "bags"]
+for thing in things:
+    print(thing)
+
+#PSEUDO CODE
+#create a list of items
+#create a starting point for iteration
+#while starting point is still less than the list
+
